444dd1a3bf7d8ba3b7a1320333629b773aa48b44
ordidxzero <ordidxzero@gmail.com>
2025-10-31 11:28:26 +0900

    feat: Replace ESBuild with vite

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { resolve } from "path";
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            formats: ['es'],
+            fileName: 'index',
+        },
+        minify: true,
+    },
+    plugins: [dts({ rollupTypes: true })]
+})
