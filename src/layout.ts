5e08354c10e965338814944faedaec967fec4d1e
ordidxzero <ordidxzero@gmail.com>
2025-12-05 19:02:16 +0900

    feat: CalendarLayoutEngine에 posFromTime, timeFromPos를 추가

--- src/layout.ts
+++ src/layout.ts
@@ -1,5 +1,6 @@
 import { type Duration } from "./date";
 import type { EngineEntryRef } from "./engine";
+import type { ScheduledEntry } from "./entry";
 import { sortEntriesWithDuration } from "./utils";
 
 export interface LayoutState {
@@ -57,5 +58,49 @@ export class CalendarLayoutEngine {
     }
 
     return state;
+  }
+
+  /**
+   * Entry의 시작 시간을 기반으로 Y 좌표(픽셀)를 계산합니다.
+   * Day/Week TimeGrid에서 Entry의 확정된 렌더링 위치를 구할 때 사용됩니다.
+   * @param entry - SchduledEntry
+   * @param dayStartsAt - 하루가 시작하는 시간
+   * @param blockHeight - 한 시간 단위의 블럭의 높이 (px)
+   */
+  static posFromTime(
+    entry: ScheduledEntry,
+    dayStartsAt: number,
+    blockHeight: number = 84
+  ) {
+    const hours = entry.startDate.getHours();
+    const minutes = entry.startDate.getMinutes();
+
+    const pieceHeight = blockHeight / 12; // 5분 단위로 움직임
+    const minutesPerPiece = 60 / 12;
+
+    const hourOffset = (hours - dayStartsAt) * blockHeight;
+    const minuteOffset = (minutes / minutesPerPiece) * pieceHeight;
+
+    return hourOffset + minuteOffset;
+  }
+
+  /**
+   * Day/Week 뷰에서 위치에 따른 시(hour)와 분(minute)을 계산하는 기능을 제공합니다.
+   * 이 함수는 Entry를 드래그하여 시간을 변경하고자할 때 사용합니다.
+   * @param top - 드래그 중인Entry의 top 값
+   * @param blockHeight - 한 시간 단위의 블럭의 높이 (px)
+   * @returns
+   */
+  static timeFromPos(
+    top: number,
+    dayStartsAt: number,
+    blockHeight: number = 84
+  ): [number, number] {
+    const pieceHeight = blockHeight / 12;
+    const unit = 60 / 12;
+    const minutes = Math.round(top / pieceHeight) * unit;
+    const hour = Math.floor(minutes / 60);
+    const minute = minutes - hour * 60;
+    return [hour + dayStartsAt, minute];
   }
 }
