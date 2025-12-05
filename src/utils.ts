43879be9cb0ea30a9d66a1d1d18083733dce07b3
ordidxzero <ordidxzero@gmail.com>
2025-12-05 21:45:11 +0900

    fix: bug

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,30 @@
-import type { Duration } from "./date";
+import { DATETIME_UNIT, type Duration } from "./date";
 import type { EngineEntryRef } from "./engine";
 
 export const sortEntries = (entryA: EngineEntryRef, entryB: EngineEntryRef) => {
-  const startPos = entryA.startDate.diff(entryB.startDate);
+  if (entryA.allDay && !entryB.allDay) return -1;
+  if (!entryA.allDay && entryB.allDay) return 1;
+  const startPos = entryA.startDate.diff(entryB.startDate, DATETIME_UNIT.HOUR);
   if (startPos !== 0) return startPos;
 
-  return entryB.endDate.diff(entryA.endDate);
+  return entryB.endDate.diff(entryA.endDate, DATETIME_UNIT.HOUR);
 };
 
 export const sortEntriesWithDuration = (duration: Duration) => {
   const sort = (entryA: EngineEntryRef, entryB: EngineEntryRef) => {
-    const startPosA = entryA.startDate.diff(duration.startDate);
-    const startPosB = entryB.startDate.diff(duration.startDate);
+    if (entryA.allDay && !entryB.allDay) return -1;
+    if (!entryA.allDay && entryB.allDay) return 1;
+    const startPosA = entryA.startDate.diff(
+      duration.startDate,
+      DATETIME_UNIT.HOUR
+    );
+    const startPosB = entryB.startDate.diff(
+      duration.startDate,
+      DATETIME_UNIT.HOUR
+    );
 
     if ((startPosA <= 0 && startPosB <= 0) || startPosA == startPosB) {
-      return entryB.endDate.diff(entryA.endDate);
+      return entryB.endDate.diff(entryA.endDate, DATETIME_UNIT.HOUR);
     }
 
     return startPosA - startPosB;
